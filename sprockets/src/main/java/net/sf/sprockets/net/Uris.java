538d9971488153d1668a4528456adb19fec4aabc
pushbit <pushbit@gmail.com>
2015-05-08 17:10:16 +0200

    adding {groupBy{,Having{,Limit}} and check for empty params in mailto()

--- sprockets/src/main/java/net/sf/sprockets/net/Uris.java
+++ sprockets/src/main/java/net/sf/sprockets/net/Uris.java
@@ -1,16 +1,16 @@
 /*
- * Copyright 2013-2014 pushbit <pushbit@gmail.com>
- * 
+ * Copyright 2013-2015 pushbit <pushbit@gmail.com>
+ *
  * This file is part of Sprockets.
- * 
+ *
  * Sprockets is free software: you can redistribute it and/or modify it under the terms of the GNU
  * Lesser General Public License as published by the Free Software Foundation, either version 3 of
  * the License, or (at your option) any later version.
- * 
+ *
  * Sprockets is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License along with Sprockets. If
  * not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,6 +22,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.net.Uri.Builder;
 import android.provider.BaseColumns;
+import android.text.TextUtils;
 
 import com.google.common.base.Joiner;
 
@@ -31,6 +32,8 @@ import java.util.List;
 
 import static android.provider.BaseColumns._ID;
 import static net.sf.sprockets.content.Content.CALLER_IS_SYNCADAPTER;
+import static net.sf.sprockets.content.Content.GROUP_BY;
+import static net.sf.sprockets.content.Content.HAVING;
 import static net.sf.sprockets.content.Content.LIMIT;
 import static net.sf.sprockets.content.Content.NOTIFY_CHANGE;
 
@@ -49,10 +52,59 @@ public class Uris {
     }
 
     /**
+     * Append a {@link Content#GROUP_BY group_by} query parameter to the URI.
+     *
+     * @since 2.4.0
+     */
+    public static Uri groupBy(Uri uri, String groupBy) {
+        return groupByHavingLimit(uri, groupBy, null, null);
+    }
+
+    /**
+     * Append {@link Content#GROUP_BY group_by} and {@link Content#HAVING having} query parameters
+     * to the URI.
+     *
+     * @since 2.4.0
+     */
+    public static Uri groupByHaving(Uri uri, String groupBy, String having) {
+        return groupByHavingLimit(uri, groupBy, having, null);
+    }
+
+    /**
+     * Append a {@link Content#LIMIT limit} query parameter to the URI.
+     *
+     * @since 2.4.0
+     */
+    public static Uri limit(Uri uri, int limit) {
+        return limit(uri, String.valueOf(limit));
+    }
+
+    /**
      * Append a {@link Content#LIMIT limit} query parameter to the URI.
      */
     public static Uri limit(Uri uri, String limit) {
-        return uri.buildUpon().appendQueryParameter(LIMIT, limit).build();
+        return groupByHavingLimit(uri, null, null, limit);
+    }
+
+    /**
+     * Append {@link Content#GROUP_BY group_by}, {@link Content#HAVING having}, and
+     * {@link Content#LIMIT limit} query parameters to the URI. Any null or empty parameters are
+     * skipped.
+     *
+     * @since 2.4.0
+     */
+    public static Uri groupByHavingLimit(Uri uri, String groupBy, String having, String limit) {
+        Builder builder = uri.buildUpon();
+        if (!TextUtils.isEmpty(groupBy)) {
+            builder.appendQueryParameter(GROUP_BY, groupBy);
+        }
+        if (!TextUtils.isEmpty(having)) {
+            builder.appendQueryParameter(HAVING, having);
+        }
+        if (!TextUtils.isEmpty(limit)) {
+            builder.appendQueryParameter(LIMIT, limit);
+        }
+        return builder.build();
     }
 
     /**
@@ -87,8 +139,8 @@ public class Uris {
     }
 
     /**
-     * Get a {@code mailto} Uri with the headers. Any parameter can be null and it will be skipped.
-     * The subject and body will be encoded.
+     * Get a {@code mailto} Uri with the headers. Any null or empty parameters are skipped. The
+     * subject and body will be encoded.
      */
     public static Uri mailto(List<String> to, List<String> cc, List<String> bcc, String subject,
                              String body) {
@@ -99,25 +151,33 @@ public class Uris {
                 + (encSubject != null ? encSubject.length() : 0)
                 + (encBody != null ? encBody.length() : 0));
         Joiner joiner = Joiner.on(',');
-        if (to != null) {
+        if (to != null && !to.isEmpty()) {
             joiner.appendTo(ssp, to);
         }
-        boolean queryStart = true;
-        if (cc != null) {
-            joiner.appendTo(ssp.append(queryStart ? '?' : '&').append("cc="), cc);
-            queryStart = false;
+        boolean start = true;
+        if (cc != null && !cc.isEmpty()) {
+            joiner.appendTo(ssp.append(separator(start)).append("cc="), cc);
+            start = false;
         }
-        if (bcc != null) {
-            joiner.appendTo(ssp.append(queryStart ? '?' : '&').append("bcc="), bcc);
-            queryStart = false;
+        if (bcc != null && !bcc.isEmpty()) {
+            joiner.appendTo(ssp.append(separator(start)).append("bcc="), bcc);
+            start = false;
         }
-        if (encSubject != null) {
-            ssp.append(queryStart ? '?' : '&').append("subject=").append(encSubject);
-            queryStart = false;
+        if (!TextUtils.isEmpty(encSubject)) {
+            ssp.append(separator(start)).append("subject=").append(encSubject);
+            start = false;
         }
-        if (encBody != null) {
-            ssp.append(queryStart ? '?' : '&').append("body=").append(encBody);
+        if (!TextUtils.isEmpty(encBody)) {
+            ssp.append(separator(start)).append("body=").append(encBody);
+            start = false;
         }
         return new Builder().scheme("mailto").encodedOpaquePart(ssp.toString()).build();
+    }
+
+    /**
+     * Get a separator that starts or continues a query string.
+     */
+    private static char separator(boolean start) {
+        return start ? '?' : '&';
     }
 }
